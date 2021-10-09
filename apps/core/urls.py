52418ac102a16b6157f53bf97dfd7b39f0b14fb5
jadonhoang <jadonhoang@gmail.com>
2021-10-09 12:17:39 -0700

    added panels

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from apps.core import views
 
 urlpatterns = [
-    path('', views.view_panels, name='home'),
+    path('', views.view_panels, name='home_panels'),
     path('all_repos/', views.allrepos, name='all_repos'),
     path('repos_sizes/', views.repos_size, name='repos_sizes'),
     path('panel_details/<panel_id>', views.panel_details),
